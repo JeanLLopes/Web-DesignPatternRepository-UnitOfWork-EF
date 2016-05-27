2f0c59731b3391bad70437946ded9ca98e23f285
JeanLLopes <jean.llopes@outlook.com>
2016-05-26 22:34:28 -0300

    Add Models

---
+++ Web-UnitOfWork-EF.Model/RecipeModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Web_UnitOfWork_EF.Model
+{
+    public class RecipeModel
+    {
+        public Int32 Id { get; set; }
+        public String Title { get; set; }
+        public String Ingredients { get; set; }
+        public String Steps { get; set; }
+        public Guid UserId { get; set; }
+    }
+}
---
+++ Web-UnitOfWork-EF.Model/UserModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Web_UnitOfWork_EF.Model
+{
+    public class UserModel
+    {
+            public Int32 Id { get; set; }
+            public String Nome { get; set; }
+            public String Email { get; set; }
+            public DateTime DtNascimento { get; set; }
+            //public virtual IList<RecipeModel> Recipes { get; set; }
+    }
+}
